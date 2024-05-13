fca7946a562cdeccfd1c4ce42af808eba3b4412c
fmfahath <fmfahath.dev@gmail.com>
2024-05-13 14:00:42 +0530

    search bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import searchIcon from './assets/search.png';
+import clearIcon from './assets/clear.png';
+import cloudyIcon from './assets/cloudy.png';
+import drizzleIcon from './assets/drizzle.png';
+import humidityIcon from './assets/humidity.png';
+import rainyIcon from './assets/rainy.png';
+import snowIcon from './assets/snow.png';
+import sunnyIcon from './assets/sunny.png';
+import windyIcon from './assets/windy.png';
 
 function App() {
 
   return (
     <>
-      <h1>weather App</h1>
+      <div className="container">
+        <div className="input-container">
+          <input type="text" className='cityInput' placeholder='Search City' />
+          <div className='search-icon'>
+            <img src={searchIcon} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
